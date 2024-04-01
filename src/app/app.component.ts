f3626b24b7a3d0fd948a2e815abc11d02ac734f5
hantaray <info@alexander-hantke.com>
2024-04-01 12:55:33 +0200

    added cart button to app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { SingleEvent } from './single-event';
 import { HomeComponent } from './home/home.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, RouterModule],
+  imports: [HomeComponent, RouterModule, MatMenuModule, MatIconModule, MatListModule],
   template: `
   <main>
     <a [routerLink]="['/']">
       <header class="brand-name">
         <img class="brand-logo" src="/assets/eventmood_logo.png" alt="EventMood logo" aria-hidden="true" />
+        <div class="menu-button">
+        <button class="shopping-cart-button" mat-icon-button [matMenuTriggerFor]="menu">
+          <mat-icon>shopping_cart</mat-icon>
+          <!-- Display the count of items in the cart -->
+          <span class='badge badge-warning' id='lblCartCount' *ngIf="cartContentCount > 0">{{cartContentCount}}</span>
+        </button>
+        <!-- Shopping cart menu -->
+        <mat-menu #menu="matMenu">
+          <h2 class="cart">Your events:</h2>
+          <p class="cart" *ngIf="cartContentCount == 0">There are no events in your cart</p>
+          <!-- Display events in the cart -->
+          <mat-list class="cart" role="list" *ngFor="let singleEvent of eventsInCartList">
+            <p>{{singleEvent.title}}</p><button mat-icon-button (click)="removeFromCart(singleEvent)">X</button>
+          </mat-list>
+        </mat-menu>
+      </div>
       </header>
     </a>
     <section class="content">
@@ -23,4 +43,17 @@ import { HomeComponent } from './home/home.component';
 })
 export class AppComponent {
   title = 'events';
+  cartContentCount: number = 0;
+  filteredEventsList: SingleEvent[] = [];
+  eventsInCartList: SingleEvent[] = [];
+
+  removeFromCart(event: SingleEvent) {
+    this.eventsInCartList = this.eventsInCartList.filter((e) => e._id !== event._id);
+    this.filteredEventsList.push(event);
+    // Sort the events by date
+    this.filteredEventsList = this.filteredEventsList.sort((a, b) => (a.date < b.date ? -1 : 1));
+
+    // Update the count of items in the cart
+    this.cartContentCount = this.eventsInCartList.length;
+  }
 }
